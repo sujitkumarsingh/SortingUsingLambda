4943f6fd2489e401ddbe1972466011bd34bbd657
Gary Liu <gary.liu1119@gmail.com>
2015-12-03 12:09:32 -0500

    First commit from Anypoint Studio

--- POC/workspace/sfdc-api/src/main/java/com/tcc/transformer/sfdc/SfdcQueryBuilder.java
+++ POC/workspace/sfdc-api/src/main/java/com/tcc/transformer/sfdc/SfdcQueryBuilder.java
@@ -2,6 +2,7 @@ package com.tcc.transformer.sfdc;
 
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
+import org.mule.module.http.internal.ParameterMap;
 import org.mule.transformer.AbstractMessageTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,9 +15,11 @@ public class SfdcQueryBuilder extends AbstractMessageTransformer {
 	public Object transformMessage(MuleMessage message, String outputEncoding)
 			throws TransformerException {
 		logger.info("SfdcQueryBuilder: info");
-		String queryParams [] = message.getInboundProperty("http.query.params");
+		ParameterMap httpParams = (ParameterMap) message.getInboundProperty("http.query.params");
+		String mobileNumber = httpParams.get("mobileNumber");
+		logger.info("Mobile Number=" + mobileNumber);
 		
-		return message;
+		return "MobilePhone =" + "\'" + mobileNumber + "\'";
 	}
 	
 
